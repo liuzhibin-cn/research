3d21fb77009814137eaa8c837b404448f5efa253
liuzhibin-cn <riccc.cn@gmail.com>
2018-11-29 14:09:39 +0800

    preview on git

---
+++ react-js/build/precache-manifest.b68385e5ad16ddfd4c59fab5b36a254f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c2fbb74dfc17fabc51c1",
+    "url": "/static/js/main.c2fbb74d.chunk.js"
+  },
+  {
+    "revision": "44acaf56cbe5b6f9e99f",
+    "url": "/static/js/1.44acaf56.chunk.js"
+  },
+  {
+    "revision": "c2fbb74dfc17fabc51c1",
+    "url": "/static/css/main.cbd62643.chunk.css"
+  },
+  {
+    "revision": "44acaf56cbe5b6f9e99f",
+    "url": "/static/css/1.1bbcb962.chunk.css"
+  },
+  {
+    "revision": "1fc8fc6e9e728e51a238ed4a0d57216a",
+    "url": "/index.html"
+  }
+];
